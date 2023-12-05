5d17fcf96f7f6d6a285521d473f77f8c7c270d47
Martin Alejandro Mammana <martin.ale590@gmail.com>
2023-12-05 12:36:57 -0300

    created default layout with auth

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,5 @@
+async function Page() {
+  return <h1 className="head-text">onboarding</h1>;
+}
+
+export default Page;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,35 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "komments",
+  description: "a Next.JS comments app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
